a2de92ae5fc7c53f8387b85057b7b2ef92f853bd
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 09:49:09 +0530

    Create multi line commit c++.c

---
+++ multi line commit c++.c
@@ -0,0 +1,12 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    /* this is the code of multi line so i
+    have to write 2-3 line
+    likh hi lete h yaar */
+
+    cout << " Hello is boring world ";
+    return 0;
+}
